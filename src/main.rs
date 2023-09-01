666d184b3e3e4ccdd9778667c35347026a2edfe0
César Pedraza <cpedraza@unal.edu.co>
2023-08-31 22:50:55 -0500

    first test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+struct Manager {
+    length: u64,
+}
+
+impl Manager {
+    pub fn size(&self) -> u64 {
+        self.length
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Manager;
+    #[test]
+    fn init_manager() {
+        let manager = Manager {
+            length: 0,
+        };
+
+        assert_eq!(manager.size(), 0);
+    }
 }
