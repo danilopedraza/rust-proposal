9827d1e950cda06ee675d9bdad58b87fed523c55
César Pedraza <cpedraza@unal.edu.co>
2023-09-04 21:55:02 -0500

    add allocator builder

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
+struct Block {
+    size: u64,
+}
+
 struct Allocator {
     size: u64,
+    block: Block,
 }
 
 impl Allocator {
 }
 
+fn build_allocator(size: u64) -> Allocator {
+    Allocator {
+        size,
+        block: Block {
+            size,
+        },
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::Allocator;
+    use crate::build_allocator;
     #[test]
     fn init_allocator() {
-        let allocator = Allocator {
-            size: 0,
-        };
+        let allocator = build_allocator(0);
 
         assert_eq!(allocator.size, 0);
+    }
+
+    #[test]
+    fn init_allocator_with_first_block() {
+        let allocator = build_allocator(1);
+
+        assert_eq!(allocator.block.size, 1);
     }
 }
