c398d5c247f91d5d7b4192d2dd3c932814836318
César Pedraza <cpedraza@unal.edu.co>
2023-09-04 21:42:36 -0500

    change struct name again

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 struct Allocator {
-    length: u64,
+    size: u64,
 }
 
 impl Allocator {
-    pub fn size(&self) -> u64 {
-        self.length
-    }
 }
 
 #[cfg(test)]
@@ -14,9 +11,9 @@ mod tests {
     #[test]
     fn init_manager() {
         let allocator = Allocator {
-            length: 0,
+            size: 0,
         };
 
-        assert_eq!(allocator.size(), 0);
+        assert_eq!(allocator.size, 0);
     }
 }
